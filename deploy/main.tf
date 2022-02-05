9d9c877fe6a6703c7ba33fa84fb9e435d288ac63
oakman10 <oakgroupllc@gmail.com>
2022-02-05 17:50:38 -0500

    updated files

---
+++ deploy/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0a8b4cd432b1c3063"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
